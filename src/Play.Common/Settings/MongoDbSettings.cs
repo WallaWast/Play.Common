05b4a5cfc1ad27ec43234f91445b55aab5933d8f
Wallace Torres <wallacewst@gmail.com>
2023-11-12 18:07:57 -0800

    Added CosmosDB support

--- src/Play.Common/Settings/MongoDbSettings.cs
+++ src/Play.Common/Settings/MongoDbSettings.cs
@@ -2,10 +2,16 @@ namespace Play.Common.Settings
 {
     public class MongoDbSettings
     {
+        private string connectionString;
+
         public string Host { get; init; }
 
         public string Port { get; init; }
 
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string ConnectionString
+        {
+            get { return string.IsNullOrWhiteSpace(connectionString) ? $"mongodb://{Host}:{Port}" : connectionString; }
+            init { connectionString = value; }
+        }
     }
 }
