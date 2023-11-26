d64e7f700ae67ba75c07bee2794b432d57ee01de
Wallace Torres <wallacewst@gmail.com>
2023-11-26 15:42:40 -0800

    Added AddSeqLogging extension method

---
+++ src/Play.Common/Settings/SeqSettings.cs
@@ -0,0 +1,14 @@
+namespace Play.Common.Settings;
+
+public class SeqSettings
+{
+    public string Host { get; init; }
+    public int Port { get; init; }
+    public string ServerUrl
+    {
+        get
+        {
+            return $"http://{Host}:{Port}";
+        }
+    }
+}
